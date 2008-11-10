ef4a4378bcb61fbd76da8e5a2ce2f556573d65fe
ryan.justin.smith <ryan.justin.smith@56ce7a00-96f4-11dd-97c4-0f6cf19f92f3>
2008-11-10 16:23:10 +0000

    Adding first unit tests.

--- branches/0.18/src/test/java/com/powerset/heritrix/writer/test/TestHBaseWriter.java
+++ branches/0.18/src/test/java/com/powerset/heritrix/writer/test/TestHBaseWriter.java
@@ -3,26 +3,38 @@ package com.powerset.heritrix.writer.test;
 import java.io.IOException;
 
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.powerset.heritrix.writer.HBaseWriter;
 
 public class TestHBaseWriter {
-	String master = "locahost";
+	String master = "localhost:60000";
 	String table = "test";
 	int poolMaximumActive = 10;
 	int poolMaximumWait = 20;
 	
 	HBaseWriter hw;
 	
-	@BeforeClass()
-	public void createHBaseWriter() throws IOException {
-		hw = new HBaseWriter(master, table);
-	}
-	
+	/**
+	 * Test that bad table values cannot be used when creating an instance of HbaseWriter.
+	 * 
+	 */
 	@Test()
-	public void testHBaseWriterIntegrity() {
-		Assert.assertNotNull(hw);
+	public void testCreateHBaseWriter() throws IOException {
+		// Test 
+		try {
+			hw = new HBaseWriter(master, null);
+			Assert.assertNull(hw);
+		} catch (IllegalArgumentException e) {
+			Assert.assertNotNull(e);
+		}
+		
+		try {
+			hw = new HBaseWriter(master, "");
+			Assert.assertNull(hw);
+		} catch (IllegalArgumentException e) {
+			Assert.assertNotNull(e);
+		}
+		
 	}
 }
