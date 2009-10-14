5c2d24acf556612dfeeb8de8425a29e014ac0b1f
ryan.justin.smith <ryan.justin.smith@56ce7a00-96f4-11dd-97c4-0f6cf19f92f3>
2009-10-14 16:58:45 +0000

    tagging version 0.20.2 for release

---
+++ tags/HBASE_WRITER_0_20_2/src/test/java/com/powerset/heritrix/writer/test/TestHBaseWriterPool.java
@@ -0,0 +1,53 @@
+package com.powerset.heritrix.writer.test;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.powerset.heritrix.writer.HBaseWriterPool;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class TestHBaseWriterPool.
+ */
+public class TestHBaseWriterPool {
+	
+	/** The master. */
+	String zkQuorum = "locahost";
+	
+	int clientPort = 2181;
+	
+	/** The table. */
+	String table = "test";
+	
+	/** The pool maximum active. */
+	int poolMaximumActive = 10;
+	
+	/** The pool maximum wait. */
+	int poolMaximumWait = 20;
+
+	/** The hwp. */
+	HBaseWriterPool hwp;
+
+	/**
+	 * Creates the h base writer pool.
+	 */
+	@BeforeClass()
+	public void createHBaseWriterPool() {
+		hwp = new HBaseWriterPool(zkQuorum, clientPort, table, poolMaximumActive, poolMaximumWait);
+	}
+
+	/**
+	 * Test h base writer pool integrity.
+	 */
+	@Test()
+	public void testHBaseWriterPoolIntegrity() {
+		Assert.assertNotNull(hwp);
+		Assert.assertEquals(hwp.getNumActive(), 0);
+		Assert.assertEquals(hwp.getNumIdle(), 0);
+		Assert.assertEquals(hwp.getSerialNo().intValue(), 0);
+		Assert.assertFalse(hwp.getSettings().isCompressed());
+		Assert.assertNull(hwp.getSettings().getPrefix());
+		Assert.assertNull(hwp.getSettings().getSuffix());
+	}
+}
